2660fba116fc9e87459dbbfad86e69f6618f1ef6
g8r-b8 <g8r-b8-home-cp@users.noreply.github.com>
2019-05-21 18:00:46 -0500

    Baller, Shot Caller

---
+++ HelloPlot.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+v1 = [ 1,2,3,4,5,6]
+v2 = [1,4,9,3,32,3]
+v3 = [1,2,4,6,4,3]
+
+plt.plot(v1,v2)
+plt.plot(v1,v3)
+
+plt.title("Hello MatPlotLib!")
+plt.xlabel("v1")
+plt.ylabel("v2 and v3")
+
+plt.legend(["this is v2","this is v3"])
+
+plt.show()
+
+#print("Howdy World")
+
