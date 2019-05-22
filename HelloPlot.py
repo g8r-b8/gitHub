d44526fa36f744aee250218e8cf58bd8f89875f4
g8r-b8 <g8r-b8-home-cp@users.noreply.github.com>
2019-05-21 19:26:40 -0500

    v3

--- HelloPlot.py
+++ HelloPlot.py
@@ -1,4 +1,4 @@
-import pandas as pd
+
 from matplotlib import pyplot as plt
 v1 = [ 1,2,3,4,5,6]
 v2 = [1,4,9,3,32,3]
@@ -15,5 +15,4 @@ plt.legend(["this is v2","this is v3"])
 
 plt.show()
 
-#print("Howdy World")
 
