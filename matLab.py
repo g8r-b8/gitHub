2e6a3f6f1d508881dbb8f9ad0adcdc8f914e0d07
g8r-b8 <wingatejones@gmail.com>
2019-11-13 22:13:20 -0600

    a lil fun

---
+++ matLab.py
@@ -0,0 +1,9 @@
+def modulus(n,list):
+    count=1
+    for e in list:
+        print(e%n)
+        if count%3==0: print("\n")
+
+//insert 3xa matrix outputs mod n form if matrix
+//quick personal challenge
+
